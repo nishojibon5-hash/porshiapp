a5163814a22d1762c77dc6e45f47a82b39d80416
salman <nishojibon5@gmail.com>
2026-04-11 10:40:10 +0000

    Convert web project to Android app using Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.porshi.app',
+  appName: 'Porshi',
+  webDir: 'dist'
+};
+
+export default config;
